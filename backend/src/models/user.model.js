ef33c1809938926911c796248ebbb14c021aafb8
DaxterATI <deepanshupathak36@gmail.com>
2025-03-09 12:56:03 +0530

    add user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:[true,'Passowrd is required']
+      },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+      },
+    
+      avatar:{
+        type:String, //cloudinary url
+        required:true,
+      },
+    
+      coverImage:{
+        type:String,  //cloudinary url
+      },
+},{timestamps:true});
+
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export const User = mongoose.model("User",userSchema);
+
+
